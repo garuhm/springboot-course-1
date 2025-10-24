18edc301dc9bc6502a7afb784f3ae9b0d30c1f03
gruhmosque01 <garuhmm@gmail.com>
2025-10-24 09:47:53 -0400

    refactor

--- 01 - springboot overview/src/main/java/com/luv2code/springboot/demo/myfirstapp/rest/MyRestController.java
+++ 01 - springboot overview/src/main/java/com/luv2code/springboot/demo/myfirstapp/rest/MyRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ public class MyRestController {
     // expose "/" end point, returns "hello world!"
     // get request essentially
 
+    @Value("${test.name}")
+    private String testPropName;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello world!";
+        return testPropName;
     }
 
     @GetMapping("/secret")
