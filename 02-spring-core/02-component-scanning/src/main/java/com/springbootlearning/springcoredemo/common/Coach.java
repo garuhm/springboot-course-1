4fa063d975d3c39441b2e16f76ee7b36c6b8576a
garuhm <garuhmosquera@gmail.com>
2025-10-31 16:16:33 -0400

    component scanning

---
+++ 02-spring-core/02-component-scanning/src/main/java/com/springbootlearning/springcoredemo/common/Coach.java
@@ -0,0 +1,9 @@
+package com.springbootlearning.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Coach {
+
+    public String getDailyWorkout();
+}
