8b00178de707602b9469496cfc4157cd9aea4341
garuhm <garuhmosquera@gmail.com>
2025-11-09 18:13:57 -0500

    get by id

--- 04-rest-crud/01-rest-crud/src/main/java/com/springbootlearning/restdemo/rest/StudentRestController.java
+++ 04-rest-crud/01-rest-crud/src/main/java/com/springbootlearning/restdemo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.restdemo.rest;
 
 import com.springbootlearning.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
 
         students.add(new Student("Gabe", "Ruh"));
         students.add(new Student("Jackelyn", "Vuong-Dinh"));
         students.add(new Student("Nhi", "Thien Ma"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id){
+        return students.get(id);
     }
 }
