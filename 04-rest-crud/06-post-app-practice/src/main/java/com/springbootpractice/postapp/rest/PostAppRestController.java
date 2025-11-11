503511d0c353702cd3ddd8c07bc6e78030907135
gruhmosque01 <garuhmm@gmail.com>
2025-11-11 12:11:30 -0500

    get mappings

--- 04-rest-crud/06-post-app-practice/src/main/java/com/springbootpractice/postapp/rest/PostAppRestController.java
+++ 04-rest-crud/06-post-app-practice/src/main/java/com/springbootpractice/postapp/rest/PostAppRestController.java
@@ -6,6 +6,8 @@ import com.springbootpractice.postapp.entity.User;
 import com.springbootpractice.postapp.service.PostService;
 import com.springbootpractice.postapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tools.jackson.databind.ObjectMapper;
@@ -28,7 +30,61 @@ public class PostAppRestController {
         this.mapper = mapper;
     }
 
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
 
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable int id){
+        User result = userService.getUserById(id);
+        if(result == null){
+            // custom exception will be made later
+            throw new RuntimeException("User with id: " + id + " was not found");
+        }
+        return result;
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts(){
+        return postService.getPosts();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Post getPostById(@PathVariable int id){
+        Post result = postService.getPostById(id);
+        if(result == null){
+            // custom exception will be made later
+            throw new RuntimeException("Post with id: " + id + " was not found");
+        }
+        return result;
+    }
+
+    @GetMapping("/posts/{id}/comments")
+    public List<Comment> getPostComments(@PathVariable int id){
+        Post result = postService.getPostById(id);
+        if(result == null){
+            // custom exception will be made later
+            throw new RuntimeException("Post with id: " + id + " was not found");
+        }
+        return postService.getCommentsByPost(id);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{commentId")
+    public Comment getPostComments(@PathVariable int postId, @PathVariable int commentId){
+        Post result1 = postService.getPostById(postId);
+        if(result1 == null){
+            // custom exception will be made later
+            throw new RuntimeException("Post with id: " + postId + " was not found");
+        }
+
+        Comment result2 = postService.getCommentById(postId);
+        if(result2 == null){
+            // custom exception will be made later
+            throw new RuntimeException("COmment with id: " + postId + " was not found");
+        }
+        return postService.getCommentById(commentId);
+    }
 
     private <T> T apply(Map<String, Object> payload, T obj){
         ObjectNode originalNode = mapper.convertValue(obj,ObjectNode.class);
