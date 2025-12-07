e07289bf50ab910f838bd512e519d41b46a7c6ca
garuhm <garuhmosquera@gmail.com>
2025-12-07 10:55:58 -0500

    pw on db

---
+++ 05-rest-security/02-users-on-db-plain/src/main/java/com/springbootlearning/cruddemo/security/EmployeeSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.springbootlearning.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class EmployeeSecurityConfig {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager detailsManager = new JdbcUserDetailsManager(dataSource);
+        detailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+        detailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+        return detailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
