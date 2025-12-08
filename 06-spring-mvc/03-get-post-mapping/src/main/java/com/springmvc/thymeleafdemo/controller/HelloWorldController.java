35c8e25ec5d30047a50dbacbc600edbd85a3d4fe
gruhmosque01 <garuhmm@gmail.com>
2025-12-08 09:36:26 -0500

    06-03

---
+++ 06-spring-mvc/03-get-post-mapping/src/main/java/com/springmvc/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,47 @@
+package com.springmvc.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String letsShoutDude(HttpServletRequest req, Model model) {
+        String name = req.getParameter("studentName");
+        int num = Integer.parseInt(req.getParameter("number"));
+
+        name = name.toUpperCase();
+        String msg = "Yo! " + name;
+
+        num++;
+        model.addAttribute("message", msg);
+        model.addAttribute("number", num);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String letsShoutDude(@RequestParam("studentName") String name, @RequestParam("number") String number, Model model) {
+        int num = Integer.parseInt(number);
+
+        name = name.toUpperCase();
+        String msg = "Yo! " + name;
+
+        num++;
+        model.addAttribute("message", msg);
+        model.addAttribute("number", num);
+        return "helloworld";
+    }
+}
+
