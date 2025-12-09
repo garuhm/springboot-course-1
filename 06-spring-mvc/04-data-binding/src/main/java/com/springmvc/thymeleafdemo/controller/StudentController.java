879865db94ffb71f53f9b23f570ac398dcadfc1c
gruhmosque01 <garuhmm@gmail.com>
2025-12-08 20:42:23 -0500

    06-04 text fields

--- 06-spring-mvc/04-data-binding/src/main/java/com/springmvc/thymeleafdemo/controller/StudentController.java
+++ 06-spring-mvc/04-data-binding/src/main/java/com/springmvc/thymeleafdemo/controller/StudentController.java
@@ -1,9 +1,24 @@
 package com.springmvc.thymeleafdemo.controller;
 
+import com.springmvc.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model){
+        model.addAttribute("student", new Student());
+        return "student-form";
+    }
 
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student){
+        System.out.println(student.getFirstName() + " " + student.getLastName());
+        return "student-confirmation";
+    }
 }
 
