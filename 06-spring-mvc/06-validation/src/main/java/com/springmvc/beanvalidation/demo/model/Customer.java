dc6750b889e08e5eeb8de0f221fdb30a65dd0f3a
gruhmosque01 <garuhmm@gmail.com>
2025-12-10 13:57:36 -0500

    06-06 regular expressions

--- 06-spring-mvc/06-validation/src/main/java/com/springmvc/beanvalidation/demo/model/Customer.java
+++ 06-spring-mvc/06-validation/src/main/java/com/springmvc/beanvalidation/demo/model/Customer.java
@@ -1,9 +1,6 @@
 package com.springmvc.beanvalidation.demo.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -14,6 +11,9 @@ public class Customer {
     @Min(value=0, message = "must be greater than or equal to 0")
     @Max(value=10, message = "must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+    private String postalCode;
 
     public Customer() {
     }
@@ -40,5 +40,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
