83f556991e5d0feeebea08e00192dd5db9966cd7
gruhmosque01 <garuhmm@gmail.com>
2025-12-11 12:05:30 -0500

    06-07 custom valid annt

---
+++ 06-spring-mvc/07-custom-validation/src/main/java/com/springmvc/beanvalidation/demo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,22 @@
+package com.springmvc.beanvalidation.demo.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result = code.startsWith(coursePrefix);
+
+
+        return result;
+    }
+}
