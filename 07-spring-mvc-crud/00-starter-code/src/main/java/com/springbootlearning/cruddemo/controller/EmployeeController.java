a8ea3869628be8811886e7a1182ac5325cbbf526
gruhmosque01 <garuhmm@gmail.com>
2025-12-12 07:58:06 -0500

    07 get employees starter

---
+++ 07-spring-mvc-crud/00-starter-code/src/main/java/com/springbootlearning/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.springbootlearning.cruddemo.controller;
+
+import com.springbootlearning.cruddemo.entity.Employee;
+import com.springbootlearning.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService service){
+        employeeService = service;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees", employees);
+        return "list-employees";
+    }
+}
