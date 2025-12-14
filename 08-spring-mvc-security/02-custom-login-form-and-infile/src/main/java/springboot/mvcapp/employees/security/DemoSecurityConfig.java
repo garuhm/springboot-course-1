effc0207e8a51ebd1d054a9e36ba635c01f2bf62
garuhm <garuhmosquera@gmail.com>
2025-12-13 21:24:47 -0500

    08-02 custom login no error

--- 08-spring-mvc-security/02-custom-login-form-and-infile/src/main/java/springboot/mvcapp/employees/security/DemoSecurityConfig.java
+++ 08-spring-mvc-security/02-custom-login-form-and-infile/src/main/java/springboot/mvcapp/employees/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package springboot.mvcapp.employees.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -29,5 +31,11 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(jackelyn, nhi, gabe);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) {
+        security.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated()).formLogin(form -> form.loginPage("/login").loginProcessingUrl("/authenticate").permitAll());
+        return security.build();
     }
 }
